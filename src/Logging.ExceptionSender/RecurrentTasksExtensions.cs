2c89a97d27df8f22670e59a52ef42597c5601625
Dmitry Popov <dmitry@parsec-club.ru>
2016-07-16 00:25:43 +0300

    Extension method CatchExceptions() for logging errors in other RecurrentTasks

---
+++ src/Logging.ExceptionSender/RecurrentTasksExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace RecurrentTasks
+{
+    using System;
+
+    using Logging.ExceptionSender;
+
+    public static class RecurrentTasksExtensions
+    {
+        public static T CatchExceptions<T>(this T task, ExceptionSenderTask exceptionSender) where T : ITask
+        {
+            task.AfterRunFail += (sender, e) => exceptionSender.LogException(e.Exception);
+            return task;
+        }
+    }
+}
